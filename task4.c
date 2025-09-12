5e6f964ffff5bcfa26522e2bd3974372e1a905f7
Syed Alyan Hussain <k250648@nu.edu.pk>
2025-09-13 00:10:14 +0500

    Update task4.c

--- task4.c
+++ task4.c
@@ -5,7 +5,7 @@ int main(){
 		scanf("%d",&salary);
 		printf("Enter your Age:\n");
 		scanf("%d",&age);
-		if(attendance>=75%%){
+		if(salary>=40000){
 			if(age>=25){
 				printf("Your loan is accepted !!");
 			} else{
